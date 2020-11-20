0d55ea45bab0d5c1f5c6aae24360d88cefbdd038
root <admin.admin@mail.rugit initcd Desktop/mkdir repocd repogit init --barecd ../mkdir orinak1cd orinak1git clone /home/armine/Desktop/repogit config --global user.email admin.admin@mail.ru>
2020-11-21 00:12:40 +0400

    v2

--- duty.v
+++ duty.v
@@ -2,10 +2,12 @@
 module duty(output  reg [3:0]duty_cycle,
 	input clk,
 	input rst,
-	input en);
+	input start);
 
 reg duty_state;
+reg start_r;
 reg [8:0]cnt;
+reg en;
 
 reg trig;
 reg [6:0]load;
@@ -15,60 +17,68 @@ reg trig1;
 reg [9:0]load1;
 wire out_pulse1;
 
-tim #7 tt(.clk(clk), .rst(rst),
+tim #7 t1(.clk(clk), .rst(rst),
 	.trig(trig),.load(load), .outpulse(out_pulse));
 
-tim #10 t1(.clk(clk), .rst(rst),
+tim #10 t2(.clk(clk), .rst(rst),
 	.trig(trig1),.load(load1), .outpulse(out_pulse1));
 
 always@(posedge clk)
 begin
-if(en == 1 && rst == 0 && duty_cycle <= 10 )begin
+if( en == 1 && rst == 0 && duty_cycle <= 10 )begin
+
+	trig  <= 0;
+	trig1 <= 0;
 	
 	if(duty_state) begin
 	       	if( duty_cycle <10 && out_pulse == 1)begin
 		duty_cycle <= duty_cycle + 4'b1;
-	        end 
+		trig   <= 1;
+		trig1  <= 1; 
+	        end
                 if(duty_cycle == 4'd10 && out_pulse1 == 1 )begin
 		duty_state <= 1'b0;
+		trig  <= 1;
 	        end 
-	        end
+         end
 
 	else if(duty_state==1'b0 && out_pulse == 1)	
 	begin
-		duty_cycle = duty_cycle - 4'd1;
+		trig <= 1;
+		duty_cycle <= duty_cycle - 4'd1;
 		if (duty_cycle == 4'd0)begin
 		    duty_state <= 1'b1;
+		    en <= 0;
 		end 
 	end
-        end
-	else
-	begin
-		duty_cycle <= 4'd0;
-		duty_state <= 1'b1;	
-	end
 end
+else 
+begin
+	duty_cycle <= 4'd0;
+	duty_state <= 1'b1;
+        trig  <= 1;
+        trig1 <= 1;	
+end
+end
+
 always@(posedge clk or posedge rst)
 	if(rst)begin
-		trig  = 0;
-		load  = 0;
-		trig1 = 0;
-		load1 = 0;
+		load  = 70;
+		load1 = 320;
+	end
+
+always@(posedge clk or posedge rst)
+	if(rst)begin
+		start_r <= 0;
 	end
 	else begin
-		trig  = 1;
-		load  = 70;
-		trig1 = 1;
-		load1 = 320;
-		if(cnt == 5)begin
-			trig  = 0;
-			trig1 = 0;	
-		end
+		start_r <= start;
 	end
 always@(posedge clk or posedge rst)
-	if(cnt < 6)
-		cnt <= cnt + 1;
-	else 
-		cnt = 0;
-
+	if(rst)begin
+            en <= 0;
+	end
+	else if(start & ~start_r)begin
+		en <= 1;
+	end
 endmodule
